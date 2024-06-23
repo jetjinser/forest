4c6a956ca941c04dab4a5338ee9620c8e4a1ab38
Jinser Kafka <aimer@purejs.icu>
2024-06-24 07:42:05 +0800

    yes typos

--- nix/hook.nix
+++ nix/hook.nix
@@ -5,21 +5,6 @@
       typos = {
         enable = true;
         settings.configPath = ".typos.toml";
-        files =
-          let
-            exts = builtins.concatStringsSep "|"
-              [
-                "tree"
-                "nix"
-                "xsl"
-                "js"
-                "css"
-                "html"
-                "json"
-                "txt"
-              ];
-          in
-          ("\\.{${exts}}$");
       };
     };
     devshells.default.devshell.startup.pre-commit-hook.text = config.pre-commit.installationScript;
