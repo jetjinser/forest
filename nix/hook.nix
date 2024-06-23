afb4ecca9dedcbee467c0803c4aeb1f7dee4c8e8
Jinser Kafka <aimer@purejs.icu>
2024-06-24 07:26:37 +0800

    update hook

--- nix/hook.nix
+++ nix/hook.nix
@@ -2,7 +2,10 @@
   perSystem = { config, self', inputs', pkgs, ... }: {
     pre-commit.settings.hooks = {
       nixpkgs-fmt.enable = true;
-      typos.enable = true;
+      typos = {
+        enable = true;
+        settings.configPath = ".typos.toml";
+      };
     };
     devshells.default.devshell.startup.pre-commit-hook.text = config.pre-commit.installationScript;
   };
